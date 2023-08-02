66cbe1806d009f1b95b58f63cd699db5cfd4c37e
parksh93 <sanghyun8527@gmail.com>
2023-08-02 11:16:16 +0900

    login 기능 중간 구현

--- src/main/java/com/goalddae/controller/UserController.java
+++ src/main/java/com/goalddae/controller/UserController.java
@@ -1,9 +1,46 @@
 package com.goalddae.controller;
 
+import com.goalddae.config.jwt.TokenProvider;
+import com.goalddae.dto.AccessTokenResponseDTO;
+import com.goalddae.dto.user.LoginDTO;
+import com.goalddae.entity.User;
+import com.goalddae.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    public UserController(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder, TokenProvider tokenProvider){
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @RequestMapping("/login")
+    public AccessTokenResponseDTO login(@RequestBody LoginDTO loginDTO){
+        User userInfo = userService.getByCredentials(loginDTO.getLoginId());
+
+        if (userInfo.getPassword().equals(loginDTO.getPassword())){
+            String token = tokenProvider.generateToken(userInfo, Duration.ofHours(2));
+
+            AccessTokenResponseDTO accessTokenResponseDTO = new AccessTokenResponseDTO(token);
+
+            return accessTokenResponseDTO;
+        }else{
+            AccessTokenResponseDTO accessTokenResponseDTO = new AccessTokenResponseDTO("login fail");
+            return accessTokenResponseDTO;
+        }
+
+    }
 }
---
+++ src/main/java/com/goalddae/dto/AccessTokenResponseDTO.java
@@ -0,0 +1,12 @@
+package com.goalddae.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AccessTokenResponseDTO {
+    private String accessToken;
+}
