4c2ed4120a5a605b7a822a94aa077c9e101753bd
parksh93 <sanghyun8527@gmail.com>
2023-08-04 14:36:08 +0900

    git merge

--- src/main/java/com/goalddae/service/UserDetailService.java
+++ src/main/java/com/goalddae/service/UserDetailService.java
@@ -1,23 +1,13 @@
 package com.goalddae.service;
 
-import com.goalddae.repository.UserJPARepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public class UserDetailService implements UserDetailsService {
-    private final UserJPARepository userJPARepository;
-
-    @Autowired
-    public UserDetailService(UserJPARepository userJPARepository){
-        this.userJPARepository = userJPARepository;
-    }
-
     @Override
-    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
-        return userJPARepository.findByLoginId(loginId);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
--- src/main/java/com/goalddae/service/UserService.java
+++ src/main/java/com/goalddae/service/UserService.java
@@ -1,39 +1,4 @@
 package com.goalddae.service;
 
-import com.goalddae.entity.User;
-import com.goalddae.repository.UserJPARepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
-@Service
 public class UserService {
-    private final UserJPARepository userJPARepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Autowired
-    public UserService(UserJPARepository userJPARepository, BCryptPasswordEncoder bCryptPasswordEncoder){
-        this.userJPARepository = userJPARepository;
-        this.bCryptPasswordEncoder =bCryptPasswordEncoder;
-    }
-
-    public void save(User user){
-        User newUser = User.builder()
-                .email(user.getEmail())
-                .password(bCryptPasswordEncoder.encode(user.getPassword()))
-                .nickname(user.getNickname())
-                .gender(user.getGender())
-                .profileImgUrl(user.getProfileImgUrl())
-                .phoneNumber(user.getPhoneNumber())
-                .birth(user.getBirth())
-                .preferredCity(user.getPreferredCity())
-                .preferredArea(user.getPreferredArea())
-                .build();
-
-        userJPARepository.save(newUser);
-    }
-
-    public User getByCredentials(String loginId){
-        return userJPARepository.findByLoginId(loginId);
-    }
 }
