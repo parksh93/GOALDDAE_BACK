6568287782273d3b7c58a4430a3fb178fea30ef3
deepoceanvibe <d22pocean@gmail.com>
2023-08-07 21:25:28 +0900

    내가 쓴 글 예외처리 추가

---
+++ src/main/java/com/goalddae/exception/NotFoundPostException.java
@@ -0,0 +1,8 @@
+package com.goalddae.exception;
+
+public class NotFoundPostException extends RuntimeException{
+    public NotFoundPostException(String message){
+        super(message);
+    }
+
+}
