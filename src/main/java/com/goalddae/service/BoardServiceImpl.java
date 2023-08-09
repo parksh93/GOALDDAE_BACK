b8b2ab3996d0e471a657425bf0687b7bf11af6fe
DARUDARUDA <aidfire@naver.com>
2023-08-09 18:14:14 +0900

    업데이트 제외 게시글 CRUD 구현

--- src/main/java/com/goalddae/service/BoardServiceImpl.java
+++ src/main/java/com/goalddae/service/BoardServiceImpl.java
@@ -1,5 +1,6 @@
 package com.goalddae.service;
 
+import com.goalddae.dto.board.BoardListDTO;
 import com.goalddae.entity.CommunicationBoard;
 import com.goalddae.repository.BoardJPARepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +21,66 @@ public class BoardServiceImpl implements BoardService{
     }
 
     @Override
-    public Page<CommunicationBoard> findAll(Integer page, Integer size) {
+    public Page<BoardListDTO> findAllBoardListDTO(Integer page, Integer size) {
 
         Pageable pageable = PageRequest.of(getCalibratedPno(page), size, Sort.by("id").descending());
-        return boardJPARepository.findAll(pageable);
+        return boardJPARepository.findAllBoardListDTO(pageable);
     }
 
     @Override
     public CommunicationBoard findById(Long boardId) {
         return boardJPARepository.findById(boardId).get();
     }
+
+    @Override
+    public void deleteById(Long boardId) {
+        boardJPARepository.deleteById(boardId);
+    }
+
+    @Override
+    public void save(CommunicationBoard communicationBoard) {
+        CommunicationBoard newPost = CommunicationBoard.builder()
+                .userId(communicationBoard.getUserId())
+                .writer(communicationBoard.getWriter())
+                .title(communicationBoard.getTitle())
+                .content(communicationBoard.getContent())
+                .img1(communicationBoard.getImg1())
+                .img2(communicationBoard.getImg2())
+                .img3(communicationBoard.getImg3())
+                .img4(communicationBoard.getImg4())
+                .img5(communicationBoard.getImg5())
+                .boardSortation(communicationBoard.getBoardSortation())
+                .build();
+        boardJPARepository.save(newPost);
+    }
+
+    @Override
+    public void update(CommunicationBoard communicationBoard) {
+        
+    }
+
+//    @Override
+//    public void update(CommunicationBoard communicationBoard) {
+//        CommunicationBoard updatedPost = boardJPARepository.findById(communicationBoard.getId()).orElse(null);
+//
+//        if (updatedPost != null) {
+//            updatedPost.setUserId(communicationBoard.getUserId());
+//            updatedPost.setWriter(communicationBoard.getWriter());
+//            updatedPost.setTitle(communicationBoard.getTitle());
+//            updatedPost.setContent(communicationBoard.getContent());
+//            updatedPost.setImg1(communicationBoard.getImg1());
+//            updatedPost.setImg2(communicationBoard.getImg2());
+//            updatedPost.setImg3(communicationBoard.getImg3());
+//            updatedPost.setImg4(communicationBoard.getImg4());
+//            updatedPost.setImg5(communicationBoard.getImg5());
+//            updatedPost.setBoardSortation(communicationBoard.getBoardSortation());
+//
+//            boardJPARepository.save(updatedPost);
+//        } else {
+//            // 엔티티를 찾지 못한 경우 예외 처리 등을 수행할 수 있습니다.
+//        }
+//    }
+
 
     public int getCalibratedPno(Integer pno){
         long count = boardJPARepository.count();
