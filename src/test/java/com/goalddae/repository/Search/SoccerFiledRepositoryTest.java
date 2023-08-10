f1c621f497cdd73dedc8858ad828745acbcbbf52
SeoPBi <aktms252@naver.com>
2023-08-11 00:31:33 +0900

    메인페이지 검색기능

--- src/test/java/com/goalddae/repository/Search/SoccerFiledRepositoryTest.java
+++ src/test/java/com/goalddae/repository/Search/SoccerFiledRepositoryTest.java
@@ -1,41 +1,14 @@
-    package com.goalddae.repository.Search;
+package com.goalddae.repository.Search;
 
-    import com.goalddae.entity.SoccerFiled;
-    import com.goalddae.repository.SoccerFiledRepository;
-    import org.junit.jupiter.api.DisplayName;
-    import org.junit.jupiter.api.Test;
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.boot.test.context.SpringBootTest;
-    import org.springframework.transaction.annotation.Transactional;
+import com.goalddae.service.SoccerFieldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-    import java.util.List;
+@SpringBootTest
+public class SoccerFiledRepositoryTest {
 
-    import static org.junit.jupiter.api.Assertions.assertEquals;
-    import static org.junit.jupiter.api.Assertions.assertNotNull;
+    SoccerFieldService soccerFieldService;
 
-    @SpringBootTest
-    public class SoccerFiledRepositoryTest {
 
-        @Autowired
-        SoccerFiledRepository soccerFiledRepository;
 
-        @Test
-        @Transactional
-        @DisplayName("검색 기능을 이용한 구장 지역 조회")
-        public void FindByRegionTest() {
-            String keyword = "성남";
-
-            List<SoccerFiled> soccerFiledList = soccerFiledRepository.findByRegion(keyword);
-
-            assertNotNull(soccerFiledList);
-
-            if (!soccerFiledList.isEmpty()) {
-                assertEquals(keyword, soccerFiledList.get(0).getRegion());
-                for (SoccerFiled soccerFiled : soccerFiledList) {
-                    System.out.println("검색 결과: " + soccerFiled.getFiledName());
-                }
-            } else {
-                System.out.println("검색 결과 없음");
-            }
-        }
-    }
+}
---
+++ src/test/java/com/goalddae/service/SoccerFiledRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.goalddae.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SoccerFiledRepositoryTest {
+
+}
