490f62b30e0e5f5a62ea403173e7889fc48d3824
parksh93 <sanghyun8527@gmail.com>
2023-08-11 16:21:30 +0900

    수정:userController에 import 삭제된 상태로 올라가 재생성

--- src/main/java/com/goalddae/controller/UserController.java
+++ src/main/java/com/goalddae/controller/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
