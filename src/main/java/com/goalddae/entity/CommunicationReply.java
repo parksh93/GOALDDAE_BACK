dd8c4b86d9df7dc4bbdd7853ccd22a0821fd82da
DARUDARUDA <aidfire@naver.com>
2023-08-14 19:25:20 +0900

    추가 : api 요청시 댓글과 답글을 같이 반환하는 코드 작성(구조 변경 예정)

--- src/main/java/com/goalddae/entity/CommunicationReply.java
+++ src/main/java/com/goalddae/entity/CommunicationReply.java
@@ -30,7 +30,10 @@ public class CommunicationReply {
     private String writer; // 작성자 (닉네임)
 
     @Column
-    private long parentId; // 부모 댓글의 ID (일반댓글일경우 본인)
+    private long parentId; // 부모 댓글의 ID (일반댓글일경우 null)
+
+    @Column(nullable = false)
+    private String content; // 본문
 
     @Column(nullable = false)
     private LocalDateTime replyWriteDate;
@@ -38,23 +41,11 @@ public class CommunicationReply {
     @Column(nullable = false)
     private LocalDateTime replyUpdateDate;
 
+
     @PrePersist
     public void setTime() {
         this.replyWriteDate = LocalDateTime.now();
         this.replyUpdateDate = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    public void setUpdateTime() {
-        this.replyUpdateDate = LocalDateTime.now();
-    }
-
-    @PrePersist
-    @PreUpdate
-    public void setNullParentId() {
-        if (parentId == 0) {
-            parentId = id; // Set parentId to the same value as id if it's null
-        }
     }
 
 }
