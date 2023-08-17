498639bc7a04e9146c28880fa161217977e214dc
parksh93 <sanghyun8527@gmail.com>
2023-08-17 16:39:58 +0900

    수정: vo폴더 삭제 및 feature/15에서 ChangeUserInfoDTO merge

--- src/main/java/com/goalddae/dto/user/ChangeUserInfoDTO.java
+++ src/main/java/com/goalddae/dto/user/ChangeUserInfoDTO.java
@@ -1,4 +1,4 @@
-package com.goalddae.vo.user;
+package com.goalddae.dto.user;
 
 import com.goalddae.entity.User;
 import lombok.*;
@@ -6,19 +6,19 @@ import lombok.*;
 import java.sql.Date;
 import java.time.LocalDateTime;
 
-@Getter
 @Setter
-@Builder
+@Getter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
-public class ChangeUserInfoVO {
+public class ChangeUserInfoDTO {
     private long id;
     private String loginId;
     private String email;
     private String password;
     private String userCode;
     private String name;
+    private String nickname;
     private String gender;
     private String profileImgUrl;
     private String phoneNumber;
@@ -34,13 +34,14 @@ public class ChangeUserInfoVO {
     private int activityClass;
     private String authority;
 
-    public ChangeUserInfoVO(User user){
+    public ChangeUserInfoDTO(User user){
         this.id = user.getId();
         this.loginId = user.getLoginId();
         this.email = user.getEmail();
         this.password = user.getPassword();
         this.userCode = user.getUserCode();
         this.name = user.getName();
+        this.nickname = user.getNickname();
         this.gender = user.getGender();
         this.profileImgUrl = user.getProfileImgUrl();
         this.phoneNumber = user.getPhoneNumber();
@@ -55,5 +56,32 @@ public class ChangeUserInfoVO {
         this.preferredArea = user.getPreferredArea();
         this.activityClass = user.getActivityClass();
         this.authority = user.getAuthority();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .id(id)
+                .loginId(loginId)
+                .email(email)
+                .password(password)
+                .userCode(userCode)
+                .name(name)
+                .nickname(nickname)
+                .gender(gender)
+                .profileImgUrl(profileImgUrl)
+                .phoneNumber(phoneNumber)
+                .birth(birth)
+                .matchesCnt(matchesCnt)
+                .level(level)
+                .signupDate(signupDate)
+                .profileUpdateDate(profileUpdateDate)
+                .accountSuspersion(accountSuspersion)
+                .noShowCnt(noShowCnt)
+                .preferredCity(preferredCity)
+                .preferredArea(preferredArea)
+                .activityClass(activityClass)
+                .authority(authority)
+                .build();
+
     }
 }
