79ad60d158ccfb1cf901b3bd9c8b3a900e8512d8
jwom1 <jwom1744@kyonggi.ac.kr>
2023-08-21 15:51:57 +0900

    수정: 오류내용 수정

---
+++ src/main/java/com/goalddae/service/TeamService.java
@@ -0,0 +1,27 @@
+package com.goalddae.service;
+
+import com.goalddae.dto.team.TeamListDTO;
+import com.goalddae.entity.Team;
+
+import java.util.List;
+
+public interface TeamService {
+
+    List<TeamListDTO> findAll();
+
+    Team findTeamById(Long id);
+
+    void save(Team team);
+
+    void update(Team team);
+
+    void deleteTeamById(Long id);
+
+    List<TeamListDTO> findByTeamName(String searchTerm);
+
+    List<TeamListDTO> findByArea(String area);
+
+    List<TeamListDTO> findByRecruiting(boolean recruiting);
+
+    List<TeamListDTO> findByAreaAndRecruiting(String area, boolean recruiting);
+}
