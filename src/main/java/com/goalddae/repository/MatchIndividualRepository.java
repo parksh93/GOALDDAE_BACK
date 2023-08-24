d0ffd38c6664e5326c2c7bfa7430a57938a177bc
SeoPBi <aktms252@naver.com>
2023-08-24 12:59:23 +0900

    신현섭:수정 동적테이블 타입변경

--- src/main/java/com/goalddae/repository/MatchIndividualRepository.java
+++ src/main/java/com/goalddae/repository/MatchIndividualRepository.java
@@ -4,16 +4,16 @@ package com.goalddae.repository;
 import com.goalddae.dto.match.MatchIndividualDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.security.core.parameters.P;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
 public interface MatchIndividualRepository {
-    void createMatchIndividualTable(String matchIndividualId);
-    void dropMatchIndividualTable(String matchIndividualId);
+    void createMatchIndividualTable(@Param("id") Long matchIndividualId);
+    void dropMatchIndividualTable(@Param("id") Long matchIndividualId);
 
     // 외래키 추가
     void addForeignKeyConstraintToMatchIndividual(Long fieldId);
-
 }
--- src/main/java/com/goalddae/service/MatchService.java
+++ src/main/java/com/goalddae/service/MatchService.java
@@ -1,10 +1,12 @@
 package com.goalddae.service;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface MatchService {
-    boolean createMatchTeamTable(Long matchTeamId);
-    boolean createMatchIndividualTable(Long matchIndividualId);
-    boolean dropMatchTeamTable(Long matchTeamId);
-    boolean dropMatchIndividualTable(Long matchIndividualId);
+    boolean createMatchTeamTable(@Param("fieldId") Long fieldId);
+    boolean createMatchIndividualTable(@Param("fieldId") Long fieldId);
+    boolean dropMatchTeamTable(@Param("fieldId") Long fieldId);
+    boolean dropMatchIndividualTable(@Param("fieldId") Long fieldId);
 
     // 외래키 추가 코드
     void addForeignKeyConstraintToMatchTeam(Long fieldId);
