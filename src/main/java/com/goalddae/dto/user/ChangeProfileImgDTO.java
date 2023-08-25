f08ad61fff46919a113c4edf6ab53a12afba488c
deepoceanvibe <d22pocean@gmail.com>
2023-08-25 21:43:04 +0900

    추가 : 프로필사진수정

---
+++ src/main/java/com/goalddae/dto/user/ChangeProfileImgDTO.java
@@ -0,0 +1,13 @@
+package com.goalddae.dto.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangeProfileImgDTO {
+    private String profileImgUrl;
+}
