56bbacc0de09b6ce0c67e7df5543c4a9f7511222
SeoPBi <aktms252@naver.com>
2023-08-27 18:03:24 +0900

    신현섭: 추가 구장CRUD, 테스트코드 완료

--- src/main/java/com/goalddae/service/SoccerFieldServiceImpl.java
+++ src/main/java/com/goalddae/service/SoccerFieldServiceImpl.java
@@ -3,6 +3,7 @@ package com.goalddae.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goalddae.entity.SoccerField;
 import com.goalddae.repository.SoccerFieldRepository;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -59,7 +60,7 @@ public class SoccerFieldServiceImpl implements SoccerFieldService {
     // 객체 생성 중 에러가 발생해도 테이블에 객체가 추가되어 안되게끔 @Transactional 추가
     @Override
     @Transactional
-    public SoccerField addSoccerField(SoccerField soccerField) {
+    public SoccerField save(SoccerField soccerField) {
         SoccerField newSoccerField = soccerFieldRepository.save(soccerField);
         Long id = newSoccerField.getId();
 
@@ -75,6 +76,37 @@ public class SoccerFieldServiceImpl implements SoccerFieldService {
         return newSoccerField;
     }
 
+    // 구장 수정
+    @Override
+    public SoccerField update(SoccerField soccerField) {
+        // 아이디에 해당하는 축구장이 존재하지 않는 경우 예외 발생
+        SoccerField existingSoccerField = soccerFieldRepository.findById(soccerField.getId())
+                .orElseThrow(() -> new RuntimeException("ID에 해당하는 축구장을 찾을 수 없습니다: " + soccerField.getId()));
+
+        // 기존 엔티티의 필드를 제공된 객체에서 업데이트
+        SoccerField updateSoccerFiled = SoccerField.builder()
+                .fieldName(nullCheckOrDefault(soccerField.getFieldName(), existingSoccerField.getFieldName()))
+                .toiletStatus(nullCheckOrDefault(soccerField.getToiletStatus(), existingSoccerField.getToiletStatus()))
+                .showerStatus(nullCheckOrDefault(soccerField.getShowerStatus(), existingSoccerField.getShowerStatus()))
+                .parkingStatus(nullCheckOrDefault(soccerField.getParkingStatus(), existingSoccerField.getParkingStatus()))
+                .fieldSize(nullCheckOrDefault(soccerField.getFieldSize(), existingSoccerField.getFieldSize()))
+                .fieldImg1(nullCheckOrDefault(soccerField.getFieldImg1(), existingSoccerField.getFieldImg1()))
+                .fieldImg2(nullCheckOrDefault(soccerField.getFieldImg2(), existingSoccerField.getFieldImg2()))
+                .fieldImg3(nullCheckOrDefault(soccerField.getFieldImg3(), existingSoccerField.getFieldImg3()))
+                .reservationFee(nullCheckOrDefault(soccerField.getReservationFee(), existingSoccerField.getReservationFee()))
+                .region(nullCheckOrDefault(soccerField.getRegion(), existingSoccerField.getRegion()))
+                .build();
+
+        return soccerFieldRepository.save(updateSoccerFiled);
+    }
+
+    // 수정 입력칸이 null 인 경우 기존값 사용, null이 아닌 경우 변경
+    private <T> T nullCheckOrDefault(T inputValue, T defaultValue) {
+        return inputValue != null ? inputValue : defaultValue;
+    }
+
+
+
     // 구장 이름으로 조회
     @Override
     public SoccerField findSoccerFieldByName(String fieldName) {
@@ -83,7 +115,7 @@ public class SoccerFieldServiceImpl implements SoccerFieldService {
 
     // 구장 객체 삭제
     @Override
-    public void deleteSoccerField(long id) {
+    public void delete(long id) {
         SoccerField soccerField = soccerFieldRepository.findById(id).orElse(null);
 
         // 구장 테이블 삭제 전 동적 테이블 모두 삭제
---
+++ src/test/java/com/goalddae/controller/SoccerFieldControllerTest.java
@@ -0,0 +1,145 @@
+package com.goalddae.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goalddae.entity.SoccerField;
+import com.goalddae.repository.SoccerFieldRepository;
+import com.goalddae.service.SoccerFieldService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SoccerFieldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private SoccerFieldService soccerFieldService;
+
+    @MockBean
+    private SoccerFieldRepository soccerFieldRepository;
+
+    @Test
+    @Transactional
+    @DisplayName("검색 조건에 맞는 축구장을 찾을 수 있는지 테스트")
+    public void searchFieldsTest() throws Exception {
+        // Field 객체 생성
+        SoccerField sampleField = SoccerField.builder()
+                .id(1L)
+                .region("성남")
+                .fieldName("모란 풋살장")
+                .build();
+
+        when(soccerFieldService.searchSoccerFields(any(String.class)))
+                .thenReturn(Arrays.asList(sampleField));
+
+        mockMvc.perform(get("/search/soccerField")
+                        .param("region", "성남")
+                        .param("fieldName", "모란 풋살장"))
+                .andExpect(status().isOk())
+                .andExpect(result -> print());
+    }
+
+
+    @Test
+    @DisplayName("구장 객체 생성 테스트")
+    void saveSoccerFieldTest() throws Exception {
+        SoccerField soccerField = SoccerField.builder()
+                .fieldName("테스트 풋살장")
+                .toiletStatus(1)
+                .showerStatus(1)
+                .parkingStatus(1)
+                .fieldSize("14x16")
+                .fieldImg1("테스트이미지1")
+                .inOutWhether("실외")
+                .grassWhether("인조")
+                .region("서울")
+                .build();
+
+        when(soccerFieldService
+                .save(any(SoccerField.class)))
+                .thenReturn(soccerField);
+
+        // 구장 등록
+        mockMvc.perform(post("/soccerField/save")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(soccerField)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("fieldName", is(soccerField.getFieldName())));
+    }
+
+    @Test
+    @DisplayName("구장 객체 수정 테스트")
+    public void updateSoccerFieldTest() throws Exception {
+        String jsonContent = "{ " +
+                "\"fieldImg1\" : \"img1\", " +
+                "\"fieldImg2\" : \"img2\", " +
+                "\"fieldImg3\" : \"img3\", " +
+                "\"fieldName\" : \"변경된 구장\", " +
+                "\"fieldSize\" : \"14x15\", " +
+                "\"grassWhether\" : \"인조\", " +
+                "\"inOutWhether\" : \"실내\", " +
+                "\"toiletStatus\" : 1, " +
+                "\"region\" : \"변경된 지역\", " +
+                "\"showerStatus\" : 1, " +
+                "\"parkingStatus\" : 1, " +
+                "\"deleteStatus\" : 0 " +
+                "}";
+
+        mockMvc.perform(put("/soccerField/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("구장 객체 삭제 테스트")
+    public void deleteSoccerfieldTest() {
+        // Given
+        SoccerField soccerField = SoccerField.builder()
+                .id(1L)
+                .fieldName("테스트 풋살장")
+                .toiletStatus(1)
+                .showerStatus(1)
+                .parkingStatus(1)
+                .fieldSize("14x16")
+                .fieldImg1("테스트이미지1")
+                .inOutWhether("실외")
+                .grassWhether("인조")
+                .region("서울")
+                .build();
+
+        when(soccerFieldRepository.findById(soccerField.getId())).thenReturn(Optional.of(soccerField));
+        doNothing().when(soccerFieldService).delete(soccerField.getId());
+
+        // When
+        soccerFieldService.delete(soccerField.getId());
+
+        // Then
+        verify(soccerFieldService, times(1)).delete(anyLong());
+    }
+}
