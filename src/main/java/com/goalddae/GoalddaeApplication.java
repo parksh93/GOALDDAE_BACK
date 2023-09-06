de613b0f5e424095dc7f4c6666b97b829a9f10e9
SeoPBi <aktms252@naver.com>
2023-09-06 19:09:15 +0900

    추가: 네이버스포츠기사 크롤링 작업

--- src/main/java/com/goalddae/GoalddaeApplication.java
+++ src/main/java/com/goalddae/GoalddaeApplication.java
@@ -2,12 +2,13 @@ package com.goalddae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GoalddaeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoalddaeApplication.class, args);
 	}
-
 }
