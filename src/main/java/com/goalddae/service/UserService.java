ce0f7e1b789e0ca89a0a086de83fa2ba231b894a
deepoceanvibe <d22pocean@gmail.com>
2023-09-12 14:39:00 +0900

    추가 : 회원 탈퇴(soft delete) 구현

--- src/main/java/com/goalddae/service/UserService.java
+++ src/main/java/com/goalddae/service/UserService.java
@@ -31,4 +31,5 @@ public interface UserService {
     public boolean checkLoginIdAndEmail(RequestFindPasswordDTO requestFindPasswordDTO, HttpServletResponse response);
     public boolean changePassword(ChangePasswordDTO changePasswordDTO);
     public User findByEmail(String email);
+    public void deleteUser(long id);
 }
