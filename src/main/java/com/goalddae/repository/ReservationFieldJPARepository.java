395b316c4e7555360b634ec65f62c1a9dd124c75
DARUDARUDA <aidfire@naver.com>
2023-09-13 20:23:29 +0900

    수정 : 예약시간 확인용 메서드에 비관적 락 적용

--- src/main/java/com/goalddae/repository/ReservationFieldJPARepository.java
+++ src/main/java/com/goalddae/repository/ReservationFieldJPARepository.java
@@ -1,12 +1,15 @@
 package com.goalddae.repository;
 
 import com.goalddae.entity.ReservationField;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationFieldJPARepository extends JpaRepository<ReservationField, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<ReservationField> findBySoccerFieldIdAndReservedDate(long fieldId, LocalDateTime reservedDate);
 }
 
