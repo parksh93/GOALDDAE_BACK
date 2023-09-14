f64185f021df777fc7dccf1e7cf87936a693a687
jwom1 <jwom1744@kyonggi.ac.kr>
2023-09-14 23:23:51 +0900

    추가: 신청 조회 메서드 구현

--- src/main/java/com/goalddae/dto/team/TeamMemberDTO.java
+++ src/main/java/com/goalddae/dto/team/TeamMemberDTO.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public class TeamMemberDTO {
     private long id;
     private LocalDateTime userJoinDate;
-    private int teamManager;    // 0: 팀장    1: 팀원
+    private int teamManager;    // 0: 팀장   1: 팀원
     private long teamId;
     private long userId;
 }
