4ac733a126b7f33e9ac0a5c4cb1745a91473460d
parksh93 <sanghyun8527@gmail.com>
2023-09-26 10:53:15 +0900

    수정: jwt토큰 response 안되는 사항 수정

--- src/main/java/com/goalddae/config/jwt/JwtProperties.java
+++ src/main/java/com/goalddae/config/jwt/JwtProperties.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Setter
 @Getter
 @Component
+@Configuration
 //@ConfigurationProperties("jwt")
 public class JwtProperties {
     @Value("${jwt.issuer}")
